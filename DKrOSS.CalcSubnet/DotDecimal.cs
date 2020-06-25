73ec7062e3e945c827df18c4498a50fd44443204
Daniel Kraemer <dkraemer@dkross.org>
2020-06-25 23:37:38 +0200

    Implemented IEquatable<DotDecimal>

--- DKrOSS.CalcSubnet/DotDecimal.cs
+++ DKrOSS.CalcSubnet/DotDecimal.cs
@@ -2,12 +2,13 @@
 // All rights reserved.
 // Licensed under BSD-3-clause (https://github.com/dkraemer/calcsubnet/blob/master/LICENSE)
 
+using System;
 using System.Runtime.CompilerServices;
 using DKrOSS.CalcSubnet.Extensions;
 
 namespace DKrOSS.CalcSubnet
 {
-    public class DotDecimal : IDumpable
+    public class DotDecimal : IDumpable, IEquatable<DotDecimal>
     {
         private readonly string _dumpLabel;
 
@@ -37,6 +38,41 @@ namespace DKrOSS.CalcSubnet
         public virtual string Dump()
         {
             return $"{_dumpLabel}: {ToString()}";
+        }
+
+        public bool Equals(DotDecimal other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return Value == other.Value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return ReferenceEquals(this, obj) || obj is DotDecimal other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public static bool operator ==(DotDecimal left, DotDecimal right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(DotDecimal left, DotDecimal right)
+        {
+            return !Equals(left, right);
         }
     }
 }
