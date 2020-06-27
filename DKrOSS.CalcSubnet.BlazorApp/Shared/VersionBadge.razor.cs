72b4f0e7cd1dee632a8fc0e4dcaffd10a8166408
Daniel Kraemer <dkraemer@dkross.org>
2020-06-27 02:38:18 +0200

    Show manifest version inside the app

---
+++ DKrOSS.CalcSubnet.BlazorApp/Shared/VersionBadge.razor.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2020, Daniel Kraemer
+// All rights reserved.
+// Licensed under BSD-3-clause (https://github.com/dkraemer/calcsubnet/blob/master/LICENSE)
+
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace DKrOSS.CalcSubnet.BlazorApp.Shared
+{
+    [UsedImplicitly]
+    public partial class VersionBadge
+    {
+        private Manifest _manifest;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _manifest = await Http.GetFromJsonAsync<Manifest>("manifest.json");
+            await base.OnInitializedAsync();
+        }
+
+        [UsedImplicitly]
+        private class Manifest
+        {
+            public string Version { get; set; }
+        }
+    }
+}
